472fb7f67ac07c354cf9cc5184d41063dc54a820
Daniel Nhemihes <danielnhemihes@live.com>
2024-06-12 02:45:28 -0300

    FIX: pins references

--- sketch_bomb.ino
+++ sketch_bomb.ino
@@ -9,14 +9,14 @@
 // ----------------------------------------------------------------- //
 
 //---------- DIGITAL PINS (buttons, buzzer, timer) ----------//
-#define DISPLAY_TIMER_DIO 22
-#define DISPLAY_TIMER_CLK 23
-#define GAME_BTN_1 2
-#define GAME_BTN_2 3
-#define MOD_SMS_BTN_R 26
-#define MOD_SMS_BTN_G 27
-#define MOD_SMS_BTN_Y 28
-#define MOD_SMS_BTN_P 29
+#define DISPLAY_TIMER_DIO 23
+#define DISPLAY_TIMER_CLK 22
+#define GAME_BTN_1 50
+#define GAME_BTN_2 51
+#define MOD_GEN_BTN_R 26
+#define MOD_GEN_BTN_G 27
+#define MOD_GEN_BTN_Y 28
+#define MOD_GEN_BTN_P 29
 #define MOD_PTB_BTN 8
 #define BUZZER_PIN 9
 #define MOD_MEM_BTN_1 40
@@ -28,14 +28,14 @@
 #define ERR_LED_1 A0
 #define ERR_LED_2 A1
 #define MOD_PTB_LED_OK A2
-#define MOD_PTB_LED_C A3
-#define MOD_PTB_LED_M A4
-#define MOD_PTB_LED_Y A5
-#define MOD_SMS_LED_OK A6
-#define MOD_SMS_LED_R A7
-#define MOD_SMS_LED_Y A8
-#define MOD_SMS_LED_G A9
-#define MOD_SMS_LED_P A10
+#define MOD_PTB_LED_R A3
+#define MOD_PTB_LED_G A4
+#define MOD_PTB_LED_B A5
+#define MOD_GEN_LED_OK A6
+#define MOD_GEN_LED_P A7
+#define MOD_GEN_LED_R A8
+#define MOD_GEN_LED_G A9
+#define MOD_GEN_LED_Y A10
 #define MOD_MEM_LED_OK A11
 #define MOD_MEM_LED_1 A12
 #define MOD_MEM_LED_2 A13
@@ -68,14 +68,14 @@ void setup() {
   pinMode(ERR_LED_1, OUTPUT);
   pinMode(ERR_LED_2, OUTPUT);
   pinMode(MOD_PTB_LED_OK, OUTPUT);
-  pinMode(MOD_PTB_LED_C, OUTPUT);
-  pinMode(MOD_PTB_LED_M, OUTPUT);
-  pinMode(MOD_PTB_LED_Y, OUTPUT);
-  pinMode(MOD_SMS_LED_OK, OUTPUT);
-  pinMode(MOD_SMS_LED_R, OUTPUT);
-  pinMode(MOD_SMS_LED_Y, OUTPUT);
-  pinMode(MOD_SMS_LED_G, OUTPUT);
-  pinMode(MOD_SMS_LED_P, OUTPUT);
+  pinMode(MOD_PTB_LED_R, OUTPUT);
+  pinMode(MOD_PTB_LED_G, OUTPUT);
+  pinMode(MOD_PTB_LED_B, OUTPUT);
+  pinMode(MOD_GEN_LED_OK, OUTPUT);
+  pinMode(MOD_GEN_LED_R, OUTPUT);
+  pinMode(MOD_GEN_LED_Y, OUTPUT);
+  pinMode(MOD_GEN_LED_G, OUTPUT);
+  pinMode(MOD_GEN_LED_P, OUTPUT);
   pinMode(MOD_MEM_LED_OK, OUTPUT);
   pinMode(MOD_MEM_LED_1, OUTPUT);
   pinMode(MOD_MEM_LED_2, OUTPUT);
@@ -83,10 +83,10 @@ void setup() {
   pinMode(MOD_MEM_LED_4, OUTPUT);
   pinMode(GAME_BTN_1, INPUT);
   pinMode(GAME_BTN_2, INPUT);
-  pinMode(MOD_SMS_BTN_R, INPUT);
-  pinMode(MOD_SMS_BTN_G, INPUT);
-  pinMode(MOD_SMS_BTN_Y, INPUT);
-  pinMode(MOD_SMS_BTN_P, INPUT);
+  pinMode(MOD_GEN_BTN_R, INPUT);
+  pinMode(MOD_GEN_BTN_G, INPUT);
+  pinMode(MOD_GEN_BTN_Y, INPUT);
+  pinMode(MOD_GEN_BTN_P, INPUT);
   pinMode(MOD_PTB_BTN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(MOD_MEM_BTN_1, INPUT);
@@ -94,46 +94,56 @@ void setup() {
   pinMode(MOD_MEM_BTN_3, INPUT);
   pinMode(MOD_MEM_BTN_4, INPUT);
 
-  //startGame();
+  startGame();
 }
 
 void loop() {
-  if(serial_number != ""){
+  //if(serial_number != ""){
     updateTimer();
     updateBuzzer();
     modulePTBLoop();
-  } else {
+  //} else {
     int ptb_btn = digitalRead(MOD_PTB_BTN);
-    int ptb_sms_r = digitalRead(MOD_SMS_BTN_R);
-    int ptb_sms_g = digitalRead(MOD_SMS_BTN_G);
-    int ptb_sms_y = digitalRead(MOD_SMS_BTN_Y);
-    int ptb_sms_p = digitalRead(MOD_SMS_BTN_P);
+    int ptb_gen_r = digitalRead(MOD_GEN_BTN_R);
+    int ptb_gen_g = digitalRead(MOD_GEN_BTN_G);
+    int ptb_gen_y = digitalRead(MOD_GEN_BTN_Y);
+    int ptb_gen_p = digitalRead(MOD_GEN_BTN_P);
     int mem_btn_1 = digitalRead(MOD_MEM_BTN_1);
     int mem_btn_2 = digitalRead(MOD_MEM_BTN_2);
     int mem_btn_3 = digitalRead(MOD_MEM_BTN_3);
     int mem_btn_4 = digitalRead(MOD_MEM_BTN_4);
+    int game_btn_1 = digitalRead(GAME_BTN_1);
+    int game_btn_2 = digitalRead(GAME_BTN_2);
 
+    if(ptb_btn == LOW){
+      digitalWrite(MOD_PTB_LED_R, HIGH);
+      digitalWrite(MOD_PTB_LED_B, HIGH);
+      digitalWrite(MOD_PTB_LED_G, HIGH);
+    } else {
+      digitalWrite(MOD_PTB_LED_R, LOW);
+      digitalWrite(MOD_PTB_LED_B, LOW);
+      digitalWrite(MOD_PTB_LED_G, LOW);
+    }
 
-
-    if(ptb_sms_r == LOW){
-      digitalWrite(MOD_SMS_LED_R, HIGH);
+    if(ptb_gen_r == LOW){
+      digitalWrite(MOD_GEN_LED_R, HIGH);
     } else {
-      digitalWrite(MOD_SMS_LED_R, LOW);
+      digitalWrite(MOD_GEN_LED_R, LOW);
     }
-    if(ptb_sms_g == LOW){
-      digitalWrite(MOD_SMS_LED_G, HIGH);
+    if(ptb_gen_g == LOW){
+      digitalWrite(MOD_GEN_LED_G, HIGH);
     } else {
-      digitalWrite(MOD_SMS_LED_G, LOW);
+      digitalWrite(MOD_GEN_LED_G, LOW);
     }
-    if(ptb_sms_y == LOW){
-      digitalWrite(MOD_SMS_LED_Y, HIGH);
+    if(ptb_gen_y == LOW){
+      digitalWrite(MOD_GEN_LED_Y, HIGH);
     } else {
-      digitalWrite(MOD_SMS_LED_Y, LOW);
+      digitalWrite(MOD_GEN_LED_Y, LOW);
     }
-    if(ptb_sms_p == LOW){
-      digitalWrite(MOD_SMS_LED_P, HIGH);
+    if(ptb_gen_p == LOW){
+      digitalWrite(MOD_GEN_LED_P, HIGH);
     } else {
-      digitalWrite(MOD_SMS_LED_P, LOW);
+      digitalWrite(MOD_GEN_LED_P, LOW);
     }
 
     if(mem_btn_1 == HIGH){
@@ -157,38 +167,20 @@ void loop() {
       digitalWrite(MOD_MEM_LED_4, LOW);
     }
 
-    if(ptb_btn == HIGH){
+    if(game_btn_2 == HIGH){
       digitalWrite(ERR_LED_1, HIGH);
       digitalWrite(ERR_LED_2, HIGH); 
       digitalWrite(MOD_PTB_LED_OK, HIGH);
-      digitalWrite(MOD_PTB_LED_C, HIGH);
-      digitalWrite(MOD_SMS_LED_OK, HIGH);
-      digitalWrite(MOD_SMS_LED_R, HIGH);
-      digitalWrite(MOD_SMS_LED_Y, HIGH);
-      digitalWrite(MOD_SMS_LED_G, HIGH);
-      digitalWrite(MOD_SMS_LED_P, HIGH);
+      digitalWrite(MOD_GEN_LED_OK, HIGH);
       digitalWrite(MOD_MEM_LED_OK, HIGH);
-      digitalWrite(MOD_MEM_LED_1, HIGH);
-      digitalWrite(MOD_MEM_LED_2, HIGH);
-      digitalWrite(MOD_MEM_LED_3, HIGH);
-      digitalWrite(MOD_MEM_LED_4, HIGH);
     } else {
       digitalWrite(ERR_LED_1, LOW);
       digitalWrite(ERR_LED_2, LOW);
       digitalWrite(MOD_PTB_LED_OK, LOW);
-      digitalWrite(MOD_PTB_LED_C, LOW);
-      digitalWrite(MOD_SMS_LED_OK, LOW);
-      digitalWrite(MOD_SMS_LED_R, LOW);
-      digitalWrite(MOD_SMS_LED_Y, LOW);
-      digitalWrite(MOD_SMS_LED_G, LOW);
-      digitalWrite(MOD_SMS_LED_P, LOW);
+      digitalWrite(MOD_GEN_LED_OK, LOW);
       digitalWrite(MOD_MEM_LED_OK, LOW);
-      digitalWrite(MOD_MEM_LED_1, LOW);
-      digitalWrite(MOD_MEM_LED_2, LOW);
-      digitalWrite(MOD_MEM_LED_3, LOW);
-      digitalWrite(MOD_MEM_LED_4, LOW);
     }
-  }
+  //}
 }
 
 
@@ -306,10 +298,10 @@ void updateBuzzer() {
         noTone(BUZZER_PIN);
         break;
       case 1:
-        tone(BUZZER_PIN, 2178);
+        //tone(BUZZER_PIN, 2178);
         break;
       case 2:
-        tone(BUZZER_PIN, 1758);
+        //tone(BUZZER_PIN, 1758);
         break;
     }
   }
@@ -325,12 +317,6 @@ void updateBuzzer() {
 
 void modulePTBLoop() {
   int buttonState = digitalRead(MOD_PTB_BTN);
-
-  if(buttonState == HIGH){
-    digitalWrite(ERR_LED_1, HIGH);
-  } else {
-    digitalWrite(ERR_LED_1, LOW);
-  }
 }
 
 
